903c81aa845f4cef886507d520fffa354f842eb2
thomasprycejones <thomas@soymomo.com>
2023-05-20 00:23:09 -0400

    fix: improve create_app_bundle.sh

--- distribution/macos/create_app_bundle.sh
+++ distribution/macos/create_app_bundle.sh
@@ -1,50 +1,56 @@
 #!/bin/bash
 
+# Terminate the script if any command returns a non-zero status
 set -e
 
-PUBLISH_DIRECTORY=$1
-OUTPUT_DIRECTORY=$2
-ENTITLEMENTS_FILE_PATH=$3
+# Check if the correct number of arguments were supplied
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <PUBLISH_DIRECTORY> <OUTPUT_DIRECTORY> <ENTITLEMENTS_FILE_PATH>"
+    exit 1
+fi
+
+PUBLISH_DIRECTORY=$(readlink -f "$1")
+OUTPUT_DIRECTORY=$(readlink -f "$2")
+ENTITLEMENTS_FILE_PATH=$(readlink -f "$3")
 
 APP_BUNDLE_DIRECTORY="$OUTPUT_DIRECTORY/Ryujinx.app"
 
-rm -rf "$APP_BUNDLE_DIRECTORY"
-mkdir -p "$APP_BUNDLE_DIRECTORY/Contents"
-mkdir "$APP_BUNDLE_DIRECTORY/Contents/Frameworks"
-mkdir "$APP_BUNDLE_DIRECTORY/Contents/MacOS"
-mkdir "$APP_BUNDLE_DIRECTORY/Contents/Resources"
+# Create the directories for the .app bundle
+mkdir -p "$APP_BUNDLE_DIRECTORY/Contents" \
+         "$APP_BUNDLE_DIRECTORY/Contents/Frameworks" \
+         "$APP_BUNDLE_DIRECTORY/Contents/MacOS" \
+         "$APP_BUNDLE_DIRECTORY/Contents/Resources"
 
-# Copy executables first
+# Copy executables
 cp "$PUBLISH_DIRECTORY/Ryujinx.Ava" "$APP_BUNDLE_DIRECTORY/Contents/MacOS/Ryujinx"
 chmod u+x "$APP_BUNDLE_DIRECTORY/Contents/MacOS/Ryujinx"
 
-# Then all libraries
+# Copy all libraries
 cp "$PUBLISH_DIRECTORY"/*.dylib "$APP_BUNDLE_DIRECTORY/Contents/Frameworks"
 
-# Then resources
+# Copy resources
 cp Info.plist "$APP_BUNDLE_DIRECTORY/Contents"
 cp Ryujinx.icns "$APP_BUNDLE_DIRECTORY/Contents/Resources/Ryujinx.icns"
 cp updater.sh "$APP_BUNDLE_DIRECTORY/Contents/Resources/updater.sh"
 cp -r "$PUBLISH_DIRECTORY/THIRDPARTY.md" "$APP_BUNDLE_DIRECTORY/Contents/Resources"
 
+# Write PkgInfo
 echo -n "APPL????" > "$APP_BUNDLE_DIRECTORY/Contents/PkgInfo"
 
 # Fixup libraries and executable
-python3 bundle_fix_up.py "$APP_BUNDLE_DIRECTORY" MacOS/Ryujinx
+if ! python3 bundle_fix_up.py "$APP_BUNDLE_DIRECTORY" MacOS/Ryujinx; then
+    echo "Failed to fix up libraries and executable. Exiting."
+    exit 1
+fi
 
-# Now sign it
-if ! [ -x "$(command -v codesign)" ];
-then
-    if ! [ -x "$(command -v rcodesign)" ];
-    then
-        echo "Cannot find rcodesign on your system, please install rcodesign."
-        exit 1
-    fi
-
-    # cargo install apple-codesign
-    echo "Usign rcodesign for ad-hoc signing"
+# Check for codesign and rcodesign
+if command -v codesign >/dev/null 2>&1; then
+    echo "Using codesign for ad-hoc signing"
+    codesign --entitlements "$ENTITLEMENTS_FILE_PATH" -f --deep -s - "$APP_BUNDLE_DIRECTORY"
+elif command -v rcodesign >/dev/null 2>&1; then
+    echo "Using rcodesign for ad-hoc signing"
     rcodesign sign --entitlements-xml-path "$ENTITLEMENTS_FILE_PATH" "$APP_BUNDLE_DIRECTORY"
 else
-    echo "Usign codesign for ad-hoc signing"
-    codesign --entitlements "$ENTITLEMENTS_FILE_PATH" -f --deep -s - "$APP_BUNDLE_DIRECTORY"
+    echo "Cannot find codesign or rcodesign on your system. Please install one of them."
+    exit 1
 fi
